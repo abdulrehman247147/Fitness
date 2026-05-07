6e7e93147496d4f171e3fb8619f5e1d00ca63a89
Abdul Rehman Memon <abdul.memon@tuhh.de>
2026-05-07 17:17:30 +0200

    Adding AI Service

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -0,0 +1,33 @@
+package com.fitness.aiservice.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Document(collection = "recommendations")
+@Data
+@Builder
+public class Recommendation {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private String activityId;
+    private String activityType;
+    private String recommendation;
+    private List<String> improvements;
+    private List<String> suggestions;
+    private List<String> safety;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private double score;
+}
